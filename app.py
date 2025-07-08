13024875ccad444dbd0b4c569ee732c5d0185cdd
sylviathalia <126743392+sylviathalia@users.noreply.github.com>
2025-07-08 16:00:04 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, render_template, jsonify
+from ultralytics import YOLO
+import cv2
+import numpy as np
+import base64
+
+app = Flask(__name__)
+model = YOLO("model/best.pt")
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    file = request.files['image']
+    img = cv2.imdecode(np.frombuffer(file.read(), np.uint8), cv2.IMREAD_COLOR)
+
+    results = model(img)
+    annotated = results[0].plot()
+
+    _, buffer = cv2.imencode('.jpg', annotated)
+    encoded_img = base64.b64encode(buffer).decode('utf-8')
+    return jsonify({'result': encoded_img})
+
+if __name__ == '__main__':
+    app.run(debug=True, port=5050)
