a295cb8b317406682e2f615af6519a26a00a569e
sylviathalia <126743392+sylviathalia@users.noreply.github.com>
2025-07-08 18:16:25 +0700

    app.py

--- app.py
+++ app.py
@@ -1,16 +1,3 @@
-from flask import Flask, request, render_template, jsonify
-from ultralytics import YOLO
-import cv2
-import numpy as np
-import os
-
-app = Flask(__name__)
-model = YOLO("model/best.pt")
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
 @app.route('/predict', methods=['POST'])
 def predict():
     file = request.files['image']
@@ -19,20 +6,17 @@ def predict():
     # Run detection
     results = model(img)
 
-    # Ambil nama kelas (label) dan koordinat box
-    names = results[0].names
+    # Ambil dict label dari model
+    label_dict = model.names
+
     boxes = []
     classes = []
 
     for box in results[0].boxes:
-        xyxy = box.xyxy.cpu().numpy().tolist()[0]  # x1, y1, x2, y2
-        cls = int(box.cls.cpu().numpy()[0])        # class index
+        xyxy = box.xyxy.cpu().numpy().tolist()[0]
+        cls_idx = int(box.cls.cpu().numpy()[0])
+        cls_name = label_dict[cls_idx]
         boxes.append(xyxy)
-        classes.append(names[cls])
+        classes.append(cls_name)
 
-    # Return sebagai JSON
     return jsonify({'labels': classes, 'boxes': boxes})
-
-if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
